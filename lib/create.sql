abb14841711a8d31e4da04ed757a89b571346d84
Sara Kantor <sara.kantor.nv@gmail.com>
2020-10-05 21:12:13 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears (
+    id int NOT NULL PRIMARY KEY,
+    name varchar,
+    age integer, 
+    gender varchar,
+    color varchar,
+    temperament varchar,
+    alive integer);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "rowdy", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 3, "F", "dark brown", "fun", 1),
+(6,"Grinch", 2, "M", "Black", "mean", 1),
+(7,"Wendy", 6, "F", "Blue", "angry", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
