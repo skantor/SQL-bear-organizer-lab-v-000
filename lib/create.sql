2e10e5e856ce4d2e68eb9617cb34e36c5ea7ab2a
Sara Kantor <sara.kantor.nv@gmail.com>
2020-10-05 21:29:15 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
+    id integer PRIMARY KEY,
     name varchar,
     age integer, 
     gender varchar,
     color varchar,
     temperament varchar,
-    alive boolean,
-    id integer PRIMARY KEY
+    alive boolean
     );
